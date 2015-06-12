b683e0ddb908d0a9e5c39a607f169c6e896a9514
smallproject <j.a.villas@live.com.ph>
2015-06-12 10:35:46 +0200

    initial creation

---
+++ source/playing with java/playing with java/jscript/JavaScript1.js
@@ -0,0 +1,22 @@
+﻿var foodDemand = function(food) {
+
+    alert("I want to eat" + " " + food);
+}
+
+var greetings = function(name) {
+    alert("Hello " + name + "! How are you?");
+}
+
+var console = function(text) {
+    alert(text);
+}
+
+//var food = prompt("Hey, what do you want to eat?","Enter food name here");
+//foodDemand(food);
+
+//var name = prompt("Hello? Who are you?", "Write your name here");
+//greetings(name);
+
+//console("Hello there!");
+
+console.log("Hey! What's going on?");
